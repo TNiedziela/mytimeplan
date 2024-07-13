4beae20a099b4c2d4c44914ec9c3d19115c62e44
Tomasz Niedziela <Rouczyk@gmail.com>
2024-07-13 16:31:11 +0200

    Commit-1 Create new project.
    * Created new project

---
+++ src/main/java/com/niedzielat/mytimeplan/MytimeplanApplication.java
@@ -0,0 +1,13 @@
+package com.niedzielat.mytimeplan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MytimeplanApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MytimeplanApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/niedzielat/mytimeplan/MytimeplanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.niedzielat.mytimeplan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MytimeplanApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
